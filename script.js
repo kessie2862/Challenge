4558c0951f006ede97605d4aa46d6415a145d5ac
kessie2862 <97234029+kessie2862@users.noreply.github.com>
2022-01-23 15:56:41 +0000

    Add files via upload
    Added a js file

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.querySelector(".navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
